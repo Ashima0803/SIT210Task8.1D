cec964ca41120639fb580a785d2e4339c189f4be
Ashima0803 <132448653+Ashima0803@users.noreply.github.com>
2023-10-22 10:16:23 +0530

    Update 8.3D.py

--- 8.3D.py
+++ 8.3D.py
@@ -1,9 +1,9 @@
-import smbus2
+import smbus2 #importing necessary libraries
 import time
 
-BH1750_ADDRESS = 0x23
+BH1750_ADDRESS = 0x23 #the address of the device
 
-bus = smbus2.SMBus(1)
+bus = smbus2.SMBus(1) # Initializing the SMBus for communicating
 
 # Read light level
 def light_level():
@@ -20,25 +20,25 @@ def light_level():
         # Lux calculation
         lux = (data[0] << 8 | data[1]) / 1.2
 
-        return lux
+        return lux #the value is returned
 
     except Exception as e:
-        print(f"Error: {str(e)}")
+        print(f"Error: {str(e)}") #log if there is any error
         return None
 
-if _name_ == "_main_":
+if _name_ == "_main_": #script being read
     try:
         while True:
-            current_light = light_level()
+            current_light = light_level() #value of lux from light_level is passed into current_light
 
-            if current_light is not None:
-                print(f"Intensity of light: {current_light:.2f} lux")
-                if current_light < 100:
-                    print("Too dark, very dim light.")
-                elif current_light < 1000:
+            if current_light is not None: 
+                print(f"Intensity of light: {current_light:.2f} lux") #the light intensity is printed if the value is not none
+                if current_light < 100: #if intensity is less than 100lux 
+                    print("Too dark, very dim light.") 
+                elif current_light < 1000: #if intensity is between 100 lux and 1000 lux
                     print("Medium level, Optimum light.")
                 else:
-                    print("Too bright, very intense light.")
+                    print("Too bright, very intense light.") #if intensity is more than 100lux 
 
             time.sleep(5)
 
